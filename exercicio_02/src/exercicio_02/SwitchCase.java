8da62967f137e7708d376c1505f552b337869b88
Andersonfdc <andersonfdc47@gmail.com.br>
2022-05-14 18:26:46 -0300

    Exercicio 2.3

---
+++ exercicio_02/src/exercicio_02/SwitchCase.java
@@ -0,0 +1,40 @@
+package exercicio_02;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int op;
+		double valor = 0;
+		int qtd;
+		double preco;
+		
+		System.out.println("Digite o cod: ");
+			op = ler.nextInt();
+		System.out.println("Digite a qtd: ");
+			qtd = ler.nextInt();
+		System.out.println("Digite a preco: ");
+			preco = ler.nextInt();
+			
+		switch (op) {
+			case 1:
+				valor = qtd * preco;
+				break;
+			case 2:
+				valor = qtd - preco;
+				break;
+				
+		}
+		
+		System.out.printf("%.2f%n", valor);
+		
+		ler.close();
+		
+		
+
+	}
+
+}
